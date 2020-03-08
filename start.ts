7f34eb3d35127fdc7582d76576c0df7abc55079c
jack <jack.holmes@vacasa.com>
2020-03-08 19:59:55 +1300

    cleanup

--- start.ts
+++ start.ts
@@ -6,21 +6,18 @@ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
     // prettier-ignore
 ;(async () => {
+    const enterTypingRaceSelector = "a[title='Keyboard shortcut: Ctrl+Alt+I']"
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
 
     try {
         await page.goto("https://play.typeracer.com")
-
-        const enterTypingRaceSelector =
-            "a[title='Keyboard shortcut: Ctrl+Alt+I']"
-
-        // Wait for page to load
         await page.waitForSelector(enterTypingRaceSelector)
 
         const username = process.env.TYPERACER_USERNAME
         const password = process.env.TYPERACER_PASSWORD
 
+        // Login in to typeracer account
         if (username && password) {
             console.log("Logging in")
             await page.click(".gwt-Anchor")
@@ -35,6 +32,7 @@ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
         // Wait until race is ready to enter
         let ready = await page.$(".lightLabel")
+
         while (ready !== null) {
             console.log("Waiting for race to start...")
             await sleep(1000)
@@ -43,6 +41,7 @@ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
         console.log("Race started")
 
+        // Extract paragraph content
         const paragraph = (
             await page.$$eval(
                 "table.inputPanel tbody tr td table tbody tr td div div",
@@ -50,7 +49,7 @@ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
             )
         )[0]
 
-        // Type paragraph
+        // Start typing paragraph
         for (const letter of paragraph ?? "") {
             await page.keyboard.type(letter)
             await sleep(100)
